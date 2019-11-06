1abfed86c70611e1574f3c0f108bd02404f22a60
karim arkia <karimarkia@gmail.com>
2019-11-06 10:35:51 +0200

    first V

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// import theRouter from './routes.js'
+
+
+
+
+
+var options = {
+    // router: theRouter,
+    el: '.my-app',
+    template: `
+     <section>
+     
+    
+        </section>
+`,
+    data: {
+
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+    components: {
+
+    }
+
+
+}
+
+new Vue(options);
